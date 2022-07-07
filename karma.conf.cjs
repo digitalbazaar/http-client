1d86e5099ce4aec3e312bcb5c5b24bde548b5f86
Andrew Jones <ajones@digitalbazaar.com>
2022-07-07 14:03:24 -0400

    Remove captureConsole from karma.conf.

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -6,7 +6,6 @@ module.exports = function(config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-    captureConsole: true,
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai'],
