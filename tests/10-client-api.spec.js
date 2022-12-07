265ee3a511583fd686c9179ad59a92944ce103e4
David I. Lehn <dlehn@digitalbazaar.com>
2022-12-06 21:41:54 -0500

    Fix lint issues.

--- tests/10-client-api.spec.js
+++ tests/10-client-api.spec.js
@@ -1,7 +1,11 @@
 /*!
  * Copyright (c) 2020-2022 Digital Bazaar, Inc. All rights reserved.
  */
-import {kyPromise, httpClient, DEFAULT_HEADERS} from '../lib/index.js';
+import {
+  DEFAULT_HEADERS,
+  httpClient,
+  kyPromise
+} from '../lib/index.js';
 import isNode from 'detect-node';
 import {test} from './10-client-api.spec.common.cjs';
 
