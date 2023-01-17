b3d1b430add8cd5276ebd732d33a99f85e404adc
David I. Lehn <dlehn@digitalbazaar.com>
2023-01-17 16:30:52 -0500

    Update error test endpoint checks.
    - The random endpoints used at "dog.ceo" changed their error output.
    - Need to update to use a local server for testing.

--- tests/10-client-api.spec.common.cjs
+++ tests/10-client-api.spec.common.cjs
@@ -206,18 +206,20 @@ describe('http-client API', () => {
     }
     should.not.exist(response);
     should.exist(err);
-    err.message.should.contain('No route found');
+    err.message.should.contain('404 Not Found');
     should.exist(err.response);
     should.exist(err.response.status);
     should.exist(err.status);
     err.status.should.equal(404);
-    should.exist(err.data);
-    err.data.should.be.an('object');
-    // these are API specific from the JSON body of the response
-    err.data.should.have.keys(['status', 'message', 'code']);
-    err.data.status.should.equal('error');
-    err.data.message.should.contain('No route found');
-    err.data.code.should.equal(404);
+    // FIXME: update to use local test endpoint, dog.ceo changed its output.
+    should.not.exist(err.data);
+    //should.exist(err.data);
+    //err.data.should.be.an('object');
+    //// these are API specific from the JSON body of the response
+    //err.data.should.have.keys(['status', 'message', 'code']);
+    //err.data.status.should.equal('error');
+    //err.data.message.should.contain('No route found');
+    //err.data.code.should.equal(404);
   });
   it('handles a direct get not found error with JSON data', async () => {
     let err;
@@ -230,18 +232,20 @@ describe('http-client API', () => {
     }
     should.not.exist(response);
     should.exist(err);
-    err.message.should.contain('No route found');
+    err.message.should.contain('404 Not Found');
     should.exist(err.response);
     should.exist(err.response.status);
     should.exist(err.status);
     err.status.should.equal(404);
-    should.exist(err.data);
-    err.data.should.be.an('object');
-    // these are API specific from the JSON body of the response
-    err.data.should.have.keys(['status', 'message', 'code']);
-    err.data.status.should.equal('error');
-    err.data.message.should.contain('No route found');
-    err.data.code.should.equal(404);
+    // FIXME: update to use local test endpoint, dog.ceo changed its output.
+    should.not.exist(err.data);
+    //should.exist(err.data);
+    //err.data.should.be.an('object');
+    //// these are API specific from the JSON body of the response
+    //err.data.should.have.keys(['status', 'message', 'code']);
+    //err.data.status.should.equal('error');
+    //err.data.message.should.contain('No route found');
+    //err.data.code.should.equal(404);
   });
   if(isNode) {
     describe('Nodejs execution context', () => {
