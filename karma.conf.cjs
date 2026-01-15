539b457a11646fe18474c31872a7c209922ca3bc
David I. Lehn <dlehn@digitalbazaar.com>
2026-01-14 20:18:42 -0500

    Update to `eslint@9`.
    - `eslint@9`.
    - `eslint-config-digitalbazaar` to `@digitalbazaar/eslint-config@7`.
    -  Fix lint issues.

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -10,7 +10,7 @@ module.exports = async function(config) {
     //httpServer,
     //httpsServer,
     httpHost: testHttpHost,
-    httpsHost: testHttpsHost,
+    httpsHost: testHttpsHost
   } = await startServers();
 
   config.set({
