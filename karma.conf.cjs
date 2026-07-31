4e235fa32d11dabf62901c94b376d23dbeb0de9b
David I. Lehn <dlehn@digitalbazaar.com>
2026-07-31 17:10:25 -0400

    Update copyright lines.
    - Update years.
    - Removed "All rights reserved.".

--- karma.conf.cjs
+++ karma.conf.cjs
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2020-2023 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2026 Digital Bazaar, Inc.
  */
 
 const {startServers} = require('./tests/utils.cjs');
--- tests/10-client-api.spec.cjs
+++ tests/10-client-api.spec.cjs
@@ -1,5 +1,5 @@
 /*!
- * Copyright (c) 2020-2023 Digital Bazaar, Inc. All rights reserved.
+ * Copyright (c) 2020-2026 Digital Bazaar, Inc.
  */
 const {kyPromise, httpClient, DEFAULT_HEADERS} = require('..');
 const isNode = require('detect-node');
